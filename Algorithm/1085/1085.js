597d02dde8db37abb3d869a5e9381f5e77cf1f01
tg8685 <tg8685@gmail.com>
2022-12-27 10:50:10 +0900

    Docs: add 백준1085 직사각형에서 탈출

---
+++ Algorithm/1085/1085.js
@@ -0,0 +1,16 @@
+const FS = require("fs");
+//로컬용
+let input = FS.readFileSync("./1085.txt").toString().trim().split(" ");
+//백준용
+//let input = FS.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const X = parseInt(input[0]);
+const Y = parseInt(input[1]);
+const W = parseInt(input[2]);
+const H = parseInt(input[3]);
+const toBorderX = W - X;
+const toBorderY = H - Y;
+const array = [X, Y, toBorderX, toBorderY];
+// let result = Math.min.apply(null, array);
+let result = Math.min(...array);
+console.log(result);
---
+++ Algorithm/2920/2920.js
@@ -0,0 +1,3 @@
+require("fs").readFileSync("/dev/stdin").toString().trim();
+let a = input();
+console.log(a);
