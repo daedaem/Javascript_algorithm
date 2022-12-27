53610466e978576f1fca40d27160cbf633c443df
tg8685 <tg8685@gmail.com>
2022-12-27 10:51:18 +0900

    Docs: add 백준1085 직사각형에서 탈출

--- Algorithm/1085/1085.js
+++ Algorithm/1085/1085.js
@@ -8,8 +8,10 @@ const X = parseInt(input[0]);
 const Y = parseInt(input[1]);
 const W = parseInt(input[2]);
 const H = parseInt(input[3]);
+
 const toBorderX = W - X;
 const toBorderY = H - Y;
+
 const array = [X, Y, toBorderX, toBorderY];
 // let result = Math.min.apply(null, array);
 let result = Math.min(...array);
