c61ae99b91b198ea48dda5f301b35a8dab8fbf0b
tg8685 <tg8685@gmail.com>
2023-01-02 23:57:38 +0900

    docs: add 백준 2775

---
+++ Algorithm/백준/10926/10926.js
@@ -0,0 +1,3 @@
+//const INPUT = require("fs").readFileSync("/dev/stdin").toString().trim();
+const INPUT = require("fs").readFileSync("./10926.txt").toString().trim();
+console.log(INPUT + "??!");
---
+++ Algorithm/제주코딩 100제/1~10/1.js
@@ -0,0 +1,4 @@
+let nums = [100, 200, 300, 400, 500];
+let newarr = nums.filter((number) => number != 400 && number != 500);
+
+console.log(newarr);
