5ce146b08725b4b7a4d464a275db9d2bdc7a5f86
tg8685 <tg8685@gmail.com>
2023-01-04 11:38:48 +0900

    docs: add 제주코딩 100제 1-10

--- Algorithm/제주코딩 100제/1~10/10.js
+++ Algorithm/제주코딩 100제/1~10/10.js
@@ -1,1 +1,25 @@
+const N = prompt("숫자를 입력해주세요.");
+// const N = 5;
 
+let tree = "";
+
+for (let r = N - 1; r >= 0; r--) {
+  for (let c = 0; c <= 2 * (N - 1) - r; c++) {
+    if (c >= r && c <= 0 + 2 * (N - 1) - r) {
+      tree += "*";
+    } else tree += " ";
+  }
+  tree += "\n";
+}
+
+// for (let i = 1; i <= n; i++) {
+//   let star = "";
+//   for (let j = 1; j <= n - i; j++) {
+//     star += " ";
+//   }
+//   for (let k = 1; k <= 2 * i - 1; k++) {
+//     star += "*";
+//   }
+//   tree += star + "\n";
+// }
+console.log(tree);
