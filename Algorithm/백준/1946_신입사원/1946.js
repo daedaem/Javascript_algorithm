64c47bcb7d1b9b24b3eb8a7c41631b0a23b14f0f
daedaem <tg8685@gmail.com>
2023-03-13 13:14:38 +0900

    docs: add 백준 1946_신입사원

--- Algorithm/백준/1946_신입사원/1946.js
+++ Algorithm/백준/1946_신입사원/1946.js
@@ -6,29 +6,59 @@ const IN = require("fs")
   .split("\n");
 const T = +IN[totalIdx++];
 
-const COMP = (a, b) => {
-  if (+a[0] < +b[0]) return -1;
-  return 1;
-};
 const solve = () => {
-  let answer = 1;
   let N = +IN[totalIdx++];
-  const arr = IN.slice(totalIdx, totalIdx + N).map((el) => {
-    return el.trim().split(" ");
+  let record = Array(N + 1);
+  IN.slice(totalIdx, totalIdx + N).map((el) => {
+    const [doc, inter] = el.trim().split(" ").map(Number);
+    record[doc] = inter;
   });
-  arr.sort(COMP);
-  let tempIdx = 0;
-  let maxNumber = +arr[tempIdx][1];
-  while (N--) {
-    if (maxNumber > +arr[tempIdx][1]) {
-      maxNumber = +arr[tempIdx][1];
-      ++answer;
+  let maxNum = N + 1;
+  const answer = record.reduce((init, val) => {
+    if (maxNum > val) {
+      maxNum = val;
+      return ++init;
     }
-    tempIdx++;
-    totalIdx++;
-  }
+    return init;
+  }, 0);
+  totalIdx += N;
   console.log(answer);
 };
 for (let i = 0; i < T; i++) {
   solve();
 }
+
+// let totalIdx = 0;
+// const IN = require("fs")
+//   .readFileSync("./input.txt")
+//   .toString()
+//   .trim()
+//   .split("\n");
+// const T = +IN[totalIdx++];
+
+// const COMP = (a, b) => {
+//   if (+a[0] < +b[0]) return -1;
+//   return 1;
+// };
+// const solve = () => {
+//   let answer = 1;
+//   let N = +IN[totalIdx++];
+//   const arr = IN.slice(totalIdx, totalIdx + N).map((el) => {
+//     return el.trim().split(" ");
+//   });
+//   arr.sort(COMP);
+//   let tempIdx = 0;
+//   let maxNumber = +arr[tempIdx][1];
+//   while (N--) {
+//     if (maxNumber > +arr[tempIdx][1]) {
+//       maxNumber = +arr[tempIdx][1];
+//       ++answer;
+//     }
+//     tempIdx++;
+//     totalIdx++;
+//   }
+//   console.log(answer);
+// };
+// for (let i = 0; i < T; i++) {
+//   solve();
+// }
