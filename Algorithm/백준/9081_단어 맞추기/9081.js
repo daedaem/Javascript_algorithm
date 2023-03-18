f73dac2a32d1433479cc02e39da76fdc9bb7932b
daedaem <tg8685@gmail.com>
2023-03-19 00:13:13 +0900

    docs: 9081_단어맞추기

--- Algorithm/백준/9081_단어 맞추기/9081.js
+++ Algorithm/백준/9081_단어 맞추기/9081.js
@@ -9,25 +9,29 @@ const nextPermutation = (el) => {
   let word = el.split("");
 
   let i = word.length - 1;
-
-  while (i > 0 && word[i - 1] >= word[i]) {
-    i--;
-  }
-  if (!i) return el;
-
   let j = word.length - 1;
 
+  // 뒤에서부터 오름차순이 끊기는 인덱스 찾기
+  while (i > 0 && word[i - 1] >= word[i]) i--;
+
+  //이미 뒤에서부터 오름차순이 끝까지 되므로 제일 뒤 문자이므로 리턴
+  if (!i) return el;
+
+  // 뒤에서부터 바꿔야할 인덱스(i-1) 값보다 큰 값 인덱스 j 찾기
   while (word[i - 1] >= word[j]) j--;
 
+  // 스왑하고
   [word[j], word[i - 1]] = [word[i - 1], word[j]];
 
-  let k = word.length - 1;
-  while (i < k) {
-    [word[i], word[k]] = [word[k], word[i]];
-    i++;
-    k--;
-  }
-  return word.join("");
+  //그 뒤는 어차피 오름차순이면 바로 그담 순서
+  let front = word.slice(0, i).join("");
+  let rear = word
+    .slice(i)
+    .sort((a, b) => a.charCodeAt() - b.charCodeAt())
+    .join("");
+  word = front + rear;
+
+  return word;
 };
 
 INPUT.map((el) => {
