148565c42f722b6e86d139bb12367a03f41f7e34
daedaem <tg8685@gmail.com>
2023-04-05 13:15:46 +0900

    docs: add 백준 8979_올림픽

--- Algorithm/백준/2525_오븐 시계/2525_오븐시계.js
+++ Algorithm/백준/2525_오븐 시계/2525_오븐시계.js
@@ -7,12 +7,5 @@ let [[H, M], [C]] = require("fs")
 let time = +H * 60 + +M + +C;
 M = time % 60;
 H = Math.floor(time / 60) % 24;
-// console.log(H, M, C);
-// console.log(time);
-// H = (H + C / 60) % 24;
-// M = M + (C % 60);
-// if (M / 60 >= 1) {
-//   H += Math.floor(M / 60);
-//   M %= 60;
-// }
+
 console.log(H, M);
