1a6de5d3000e481e59a1d7288848afcc4eefa342
daedaem <tg8685@gmail.com>
2023-04-13 19:41:12 +0900

    Add 백준 6198_옥상정원꾸미기

--- Algorithm/백준/6198_옥상정원꾸미기/6198_옥상정원꾸미기.js
+++ Algorithm/백준/6198_옥상정원꾸미기/6198_옥상정원꾸미기.js
@@ -1,3 +1,26 @@
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+// const [N, ...input] = require("fs")
+//   .readFileSync(filePath)
+//   .toString()
+//   .split("\n")
+//   .map(Number);
+
+// const stack = [];
+// const visit = Array.from({ length: N }, () => 0);
+
+// let idx = 0;
+// let answer = 0;
+// while (idx < N) {
+//   while (stack.length && stack[stack.length - 1] <= input[idx]) {
+//     stack.pop();
+//   }
+//   stack.push(input[idx++]);
+
+//   answer += stack.length - 1;
+// }
+// console.log(answer);
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 const [N, ...input] = require("fs")
@@ -6,17 +29,27 @@ const [N, ...input] = require("fs")
   .split("\n")
   .map(Number);
 
-const stack = [];
-const visit = Array.from({ length: N }, () => 0);
-
-let idx = 0;
 let answer = 0;
-while (idx < N) {
-  while (stack.length && stack[stack.length - 1] <= input[idx]) {
-    stack.pop();
-  }
-  stack.push(input[idx++]);
 
-  answer += stack.length - 1;
+const st = [{ height: +input[N - 1], count: 0 }];
+
+for (let i = N - 2; i >= 0; i--) {
+  const curHeight = +input[i];
+  let curCount = 0;
+  while (st.length !== 0) {
+    const { height, count } = st[0];
+    if (curHeight > height) {
+      curCount += count + 1;
+      st.shift();
+      answer += count;
+    } else {
+      break;
+    }
+  }
+  st.unshift({ height: curHeight, count: curCount });
+}
+while (st.length !== 0) {
+  const { count } = st.shift();
+  answer += count;
 }
 console.log(answer);
