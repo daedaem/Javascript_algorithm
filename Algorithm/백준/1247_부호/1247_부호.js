6866c4519ce96f3f1f2b23efcb4a4dde5da88d4e
daedaem <tg8685@gmail.com>
2023-04-15 19:36:03 +0900

    Add 백준 1247_부호

---
+++ Algorithm/백준/1247_부호/1247_부호.js
@@ -0,0 +1,18 @@
+const input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .split("\n")
+  .map((el) => BigInt(el.trim()));
+
+let idx = 0;
+for (let i = 0; i < 3; i++) {
+  const N = parseInt(input[idx]);
+  const newarr = input.slice(++idx, idx + N);
+  const result = newarr.reduce((init, val) => (init += val), BigInt(0));
+  idx += N;
+  //   console.log(newarr);
+  if (!result) {
+    console.log(0);
+  } else if (result < 0) console.log("-");
+  else console.log("+");
+}
